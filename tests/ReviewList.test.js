5a7cec2c132a40c5fe26317e25a8e03b7bc8857b
Minji Gwak <minjigwak@gmail.com>
2020-05-03 17:40:03 -0700

    set up jest and enzyme environments

---
+++ tests/ReviewList.test.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import mockresponse from '../client/src/mockresponse.js';
+
+// import ReviewList from '../client/src/components/ReviewList.jsx';
+
+// describe('<ReviewList />', () => {
+//   it('renders ReviewList component' () => {
+//     const wrapper = shallow(<ReviewList />);
+//     expect(wrapper).toExist();
+//   })
+// })
