6c43aad433d7ad1134aadce46352b185abe5b550
Philip Hamner <philiphamner@gmail.com>
2020-05-24 23:51:59 -0700

    Adding data for all four tables to CSV files.

--- database/postgres/seedpostgres.js
+++ database/postgres/seedpostgres.js
@@ -11,7 +11,12 @@ writeRestaurants.write('restaurant_id,restaurant_name\n', 'utf8');
 
 //REVIEWS
 const writeReviews = fs.createWriteStream('./csvFiles/reviewData.csv');
-writeReviews.write('restaurant_id,restaurant_name\n', 'utf8');
+writeReviews.write('review_id,restaurant_id,guest_id,review_text,review_rating,review_date,useful_count,funny_count,cool_count,comment_text,commenter_name,comment_date,commenter_photo\n', 'utf8');
+
+//REVIEW IMAGES
+const writeReviewImages = fs.createWriteStream('./csvFiles/reviewImagesData.csv');
+writeReviewImages.write('review_id,review_image_id,review_image_url\n', 'utf8');
+
 
 
 function writeGuestsToCSV(writer, encoding, callback) {
@@ -86,5 +91,93 @@ write()
 }
 writeRestaurantsToCSV(writeRestaurants, 'utf-8', () => {
   writeRestaurants.end();
+  console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO restaurantData.csv`)
+});
+
+
+function writeReviewsToCSV(writer, encoding, callback) {
+  let i = 100;
+  let review_id = 0;
+  let guest_id = 0;
+  let restaurant_id = 0;
+
+
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      review_id += 1;
+      restaurant_id +=1;
+      guest_id += 1;
+
+      const review_text = 'tesetingtesting';
+      const review_rating = 1;
+      const review_date = 'today';
+      const useful_count = 100;
+      const funny_count = 50;
+      const cool_count = 25;
+      const comment_text = null;
+      const commenter_name = null;
+      const comment_date = null;
+      const commenter_photo = null;
+
+      const data = `${review_id},${restaurant_id},${guest_id},${review_text},${review_rating},${review_date},${useful_count},${funny_count},${cool_count},${comment_text},${commenter_name},${comment_date},${commenter_photo}\n`;
+
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+// see if we should continue, or wait
+// don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+// had to stop early!
+// write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+write()
+}
+writeReviewsToCSV(writeReviews, 'utf-8', () => {
+  writeReviews.end();
+  console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO reviewData.csv`)
+});
+
+
+
+function writeReviewImagesToCSV(writer, encoding, callback) {
+  let i = 100;
+  let review_id = 0;
+  let review_image_id = 0;
+
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      review_id += 1;
+      review_image_id += 1;
+      const review_image_url = faker.image.imageUrl();
+
+      const data = `${review_id},${review_image_id},${review_image_url}\n`;
+
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+// see if we should continue, or wait
+// don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+// had to stop early!
+// write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+write()
+}
+writeReviewImagesToCSV(writeReviewImages, 'utf-8', () => {
+  writeReviewImages.end();
   console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO restaurantData.csv`)
 });
