af31f6ef9d8be57822f73c409c5ffe6fc59780e1
Philip Hamner <philiphamner@gmail.com>
2020-05-24 23:00:01 -0700

    Add new csv file for review images data.

--- database/postgres/seedpostgres.js
+++ database/postgres/seedpostgres.js
@@ -1,22 +1,20 @@
 const faker = require('faker');
 const fs = require('fs');
 
+//GUESTS
+const writeGuests = fs.createWriteStream('./csvFiles/guestData.csv');
+writeGuests.write('guest_id,username,user_location,user_friend_count,user_photo_count,user_profile_picture,elite_user\n', 'utf8');
 
-// fs.writeFile('csvFiles/Guest_100.csv', `30,Jack,Los Angeles,400,200,300,https://www.facebook.com/photo/?fbid=10222593186123601&set=a.1646786014860,FALSE`, 'utf8', function (err) {
-//   if (err) {
-//     console.log('Some error occured - file either not saved or corrupted file saved.');
-//   } else{
-//     console.log('It\'s saved!');
-//   }
-// });
+//RESTAURANTS
+const writeRestaurants = fs.createWriteStream('./csvFiles/restaurantData.csv');
+writeRestaurants.write('restaurant_id,restaurant_name\n', 'utf8');
 
-
-
-const writeUsers = fs.createWriteStream('./csvFiles/Guest_100.csv');
-writeUsers.write('guest_id,username,user_location,user_friend_count,user_photo_count,user_profile_picture,elite_user\n', 'utf8');
+//REVIEWS
+const writeReviews = fs.createWriteStream('./csvFiles/reviewData.csv');
+writeReviews.write('restaurant_id,restaurant_name\n', 'utf8');
 
 
-function writeTenMillionUsers(writer, encoding, callback) {
+function writeGuestsToCSV(writer, encoding, callback) {
   let i = 100;
   let guest_id = 0;
 
@@ -32,7 +30,6 @@ function writeTenMillionUsers(writer, encoding, callback) {
       const user_profile_picture = faker.image.imageUrl();
       const elite_user = true;
 
-      // const data = `${id},${username},${avatar}\n`;
       const data = `${guest_id},${username},${location},${user_friend_count},${user_photo_count},${user_profile_picture},${elite_user}\n`;
 
       if (i === 0) {
@@ -51,10 +48,43 @@ function writeTenMillionUsers(writer, encoding, callback) {
   }
 write()
 }
+writeGuestsToCSV(writeGuests, 'utf-8', () => {
+  writeGuests.end();
+  console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO guestData.csv`)
+});
 
 
-// Then invoke the function with a callback telling the write to end.
-writeTenMillionUsers(writeUsers, 'utf-8', () => {
-  writeUsers.end();
-  console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO Guest_100.csv`)
+function writeRestaurantsToCSV(writer, encoding, callback) {
+  let i = 100;
+  let restaurant_id = 0;
+
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      restaurant_id += 1;
+      const restaurant_name = faker.company.companyName();
+
+
+      const data = `${restaurant_id},${restaurant_name}\n`;
+
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+// see if we should continue, or wait
+// don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+// had to stop early!
+// write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+write()
+}
+writeRestaurantsToCSV(writeRestaurants, 'utf-8', () => {
+  writeRestaurants.end();
+  console.log(`YOU HAVE SUCCESSFULLY ADDED 100 RECORDS TO restaurantData.csv`)
 });
