c4a9485da91c7a531144a703d837e40046dbe743
Philip Hamner <philiphamner@gmail.com>
2020-06-01 23:26:25 -0700

    minor tweaks, end of day.

--- generateTrafficK6.js
+++ generateTrafficK6.js
@@ -3,7 +3,9 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 100 },
+    // { duration: '2m', target: 900 },
+    { duration: '3m', target: 1000 },
+    // { duration: '5m', target: 1000 },
     // { duration: '1m30s', target: 100 },
     // { duration: '20s', target: 10 },
   ],
@@ -12,6 +14,7 @@ export let options = {
 export default function() {
   // let res = http.get('https://httpbin.org/'); can delete later.
   let id = Math.floor(Math.random() * 1400000);
+  // let id = 1500000   THIS NUM SHOULDN'T WORK, AS THERE ARE NO RESTAURANTS WITH AN ID > 1.4 MILLION
   let res = http.get(`http://localhost:8080/restaurants/${id}/comments`);
 
   check(res, { 'status was 200': r => r.status == 200 });
